e5fd5101f6bd1ca35e5e19ff784de1a3e1302b3e
mfoncho <akonumboche@gmail.com>
2022-03-16 04:17:54 -0600

    add useMediaQuery hook

--- packages/ui/src/hooks.ts
+++ packages/ui/src/hooks.ts
@@ -1,9 +1,27 @@
+import { useState, useMemo, useEffect } from "react";
 import breakpoints from "./breakpoints";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+export function useMediaQuery(query: string) {
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+        const media = window.matchMedia(query.replace(/^@media( ?)/m, ""));
+        if (media.matches !== matches) {
+            setMatches(media.matches);
+        }
+        const listener = () => setMatches(media.matches);
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    }, [matches, query]);
+
+    return matches;
+}
 
 export function useScreen() {
     const desktop = useMediaQuery(breakpoints.up("lg"));
-    const mobile = useMediaQuery(breakpoints.down("sm"));
     const tablet = useMediaQuery(breakpoints.only("md"));
-    return { mobile, tablet, desktop };
+    const mobile = useMediaQuery(breakpoints.down("sm"));
+    return useMemo(() => {
+        return { mobile, tablet, desktop };
+    }, [mobile, tablet, desktop]);
 }
