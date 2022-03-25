1baee80891f9ffef7afbca92ec9cd3eefda2af2a
mfoncho <akonumboche@gmail.com>
2022-03-24 20:20:38 -0600

    update add rrdv6 Navigate redirect semantics

--- packages/webapp/src/auth/Main.tsx
+++ packages/webapp/src/auth/Main.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Login from "./Login";
 import path from "./paths";
 import ResetPassword from "./ResetPassowrd";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 export default React.memo(() => {
     return (
         <Routes>
             <Route path={path.login} element={<Login />} />
             <Route path={path.reset} element={<ResetPassword />} />
+            <Route path="/" element={<Navigate replace to={path.login} />} />
         </Routes>
     );
 });
