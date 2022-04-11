43c40dd36273a91ea6237e7b9d85a7e5f75d7165
mfoncho <akonumboche@gmail.com>
2022-04-11 17:06:11 -0600

    add Task icons

---
+++ packages/icons/src/Task.tsx
@@ -0,0 +1,13 @@
+import { MdOutlineCheckBox as DoneOutline } from "react-icons/md";
+import { MdCheckBox as DoneSolid } from "react-icons/md";
+import { MdOutlineCheckBoxOutlineBlank as Undone } from "react-icons/md";
+import { MdOutlineAddBox as Add } from "react-icons/md";
+import { MdAddBox as AddSolid } from "react-icons/md";
+
+export default {
+    DoneSolid,
+    DoneOutline,
+    Undone,
+    Add,
+    AddSolid,
+};
