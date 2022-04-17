0b3029a3044b2e3b084baab926edea7b0cbc4f9f
mfoncho <akonumboche@gmail.com>
2022-04-17 08:46:55 -0600

    update create card popper with templete impl!

--- packages/webapp/src/workspace/Board/Column/CreateCardPopper.tsx
+++ packages/webapp/src/workspace/Board/Column/CreateCardPopper.tsx
@@ -1,45 +1,93 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Popper } from "@octal/ui";
+import React, { useState, useRef } from "react";
+import { Popper, Textarea } from "@octal/ui";
+import * as Icons from "@octal/icons";
 import { useNavigator } from "src/hooks";
 import { ColumnRecord } from "@octal/store/lib/records";
 import { useInput } from "src/utils";
 import { useColumnActions } from "@workspace/Board/hooks";
-import { useUser } from "@octal/store";
 
 interface ICreateCardPopper {
     column: ColumnRecord;
     onClose: (event: any, reason: string) => void;
 }
 
+interface IDropdownPopper {}
+
 const flip = { flipVariations: false };
+
+const TemplateDropdown = Popper.create<HTMLDivElement, IDropdownPopper>(
+    (props) => {
+        const [loading, setLoading] = useState<boolean>(false);
+
+        function handleTemplateClick(event: React.MouseEvent) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        return (
+            <Popper
+                as="div"
+                flip={flip}
+                open={props.open}
+                tabIndex={-1}
+                portal={true}
+                anchorEl={props.anchorEl}
+                placement="bottom-end"
+                data-popper="template-dropdown"
+                style={{ width: props.anchorEl?.offsetWidth }}
+                onClickAway={loading ? undefined : props.onClickAway}
+                className="focus:outline-none flex flex-col rounded-md ring-1 ring-gray-800 ring-opacity-5 max-h-56 py-2 bg-white shadow-md overflow-x-hidden">
+                <div
+                    onClick={handleTemplateClick}
+                    className="flex flex-col py-2 px-4 items-center h-[256px]"></div>
+            </Popper>
+        );
+    }
+);
 
 export default Popper.create<HTMLDivElement, ICreateCardPopper>(
     ({ column, ...props }) => {
-        const user = useUser();
-
-        const inputRef = useRef<HTMLInputElement | null>(null);
-
         const actions = useColumnActions(column);
 
         const navigator = useNavigator();
 
+        const dropdownRef = useRef<HTMLDivElement | null>(null);
+
         const [loading, setLoading] = useState<boolean>(false);
 
-        const name = useInput("", (val) => val.length >= 3);
+        const [dropdown, setDropdown] = useState<boolean>(false);
 
-        useEffect(() => {
-            if (inputRef.current) {
-                // setTimout because for some reason
-                // it won't auto focus just immediately
-                // after popper parent focus
-                setTimeout(() => {
-                    inputRef.current?.focus();
-                }, 10);
+        const name = useInput("", (val) => val.length >= 2);
+
+        function handleToggleDropdown(e: MouseEvent | React.MouseEvent) {
+            if (e.target === dropdownRef.current && e.type === "click") {
+                setDropdown((value) => !value);
             }
-        }, [inputRef]);
+        }
 
-        function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
-            if (e.key == "Enter") {
+        function handleTemplateDropdownClickAway(
+            e: MouseEvent | React.MouseEvent
+        ) {
+            if (e.target !== dropdownRef.current) {
+                setDropdown(false);
+            }
+        }
+
+        function handleClickAway(e: MouseEvent) {
+            let suggestion = e.composedPath().find((el: any) => {
+                if (el.getAttribute) {
+                    return (
+                        el.getAttribute("data-popper") === "template-dropdown"
+                    );
+                }
+                return false;
+            });
+            if (!Boolean(suggestion) && loading === false && props.onClickAway)
+                props.onClickAway(e);
+        }
+
+        function handleSubmit() {
+            if (loading == false && name.valid) {
                 actions
                     .createCard(name.value)
                     .then((data) => {
@@ -61,24 +109,44 @@ export default Popper.create<HTMLDivElement, ICreateCardPopper>(
                 tabIndex={-1}
                 anchorEl={props.anchorEl}
                 placement="bottom-end"
-                onClickAway={loading ? undefined : props.onClickAway}
-                className="focus:outline-none flex flex-col rounded-md ring-1 ring-gray-800 ring-opacity-5 max-h-56 py-2 bg-white shadow-md overflow-x-hidden">
-                <div className="flex flex-row p-2 items-center">
-                    <img
-                        className="inline-block h-8 w-8 rounded-full"
-                        alt={user.name}
-                        src={user.avatar}
-                    />
-                    <input
-                        autoFocus
-                        ref={inputRef}
+                onClickAway={loading ? undefined : handleClickAway}
+                className="focus:outline-none flex flex-col rounded-md ring-1 ring-gray-800 ring-opacity-5 max-h-56 py-2 w-[294px] bg-white shadow-md overflow-x-hidden">
+                <div className="flex flex-col py-2 px-4 items-center">
+                    <Textarea.Input
+                        autoFocus={true}
                         disabled={loading}
-                        {...name.props}
+                        value={name.value}
+                        onChange={name.setValue}
+                        onSubmit={handleSubmit}
                         placeholder="Card name"
-                        className="form-input rounded-md mx-2 font-semibold text-sm text-gray-800"
-                        onKeyPress={handleKeyPress}
+                        className="focus:border-primary-700 py-1.5 focus:shadow border-slate-500 border-2 px-2 w-full rounded-md mx-2 font-semibold text-base text-gray-900"
                     />
                 </div>
+                <div className="flex flex-col py-2 px-4 items-center">
+                    <div
+                        ref={dropdownRef}
+                        role="button"
+                        onClick={handleToggleDropdown}
+                        className="bg-slate-200 py-2 px-2.5 cursor-pointer border-slate-200 w-full rounded-md mx-2 font-semibold text-sm text-gray-900 shadow-sm hover:shadow flex flex-row justify-between">
+                        <span className="text-gray-600">Template</span>
+                        <Icons.DropdownArrows className="h-5 w-5 text-gray-500" />
+                    </div>
+                </div>
+                <div className="flex flex-col py-2 px-4 items-center">
+                    <button
+                        onClick={handleSubmit}
+                        disabled={name.valid!}
+                        className={`${
+                            name.valid ? "bg-primary-500" : "bg-primary-200"
+                        } cursor-pointer py-2 px-2.5 border-slate-200 w-full rounded-md mx-2 font-semibold text-sm text-white shadow hover:shadow-md`}>
+                        <span>Create</span>
+                    </button>
+                </div>
+                <TemplateDropdown
+                    open={dropdown}
+                    anchorEl={dropdownRef.current}
+                    onClickAway={handleTemplateDropdownClickAway}
+                />
             </Popper>
         );
     }
