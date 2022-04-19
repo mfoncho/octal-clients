ba929c8933333fe0261be1e4c21691bacb31ee88
mfoncho <akonumboche@gmail.com>
2022-04-19 15:17:11 -0600

    update delete card warning message

--- packages/webapp/src/workspace/CardDialog/DeleteWarning.tsx
+++ packages/webapp/src/workspace/CardDialog/DeleteWarning.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { Dialog, Markdown } from "@octal/ui";
+import { Dialog, Text } from "@octal/ui";
 import { CardRecord } from "@octal/store";
 
 interface IDialog {
     card: CardRecord;
     onConfirm: (e: React.MouseEvent) => void;
-}
-
-function warning(name: string) {
-    return `
-__${name}__
-
-All data related to this card will be lost
-permanently. 
-    `;
 }
 
 export default Dialog.create<IDialog>(({ card, ...props }) => {
@@ -24,7 +15,18 @@ export default Dialog.create<IDialog>(({ card, ...props }) => {
             confirm="Delete"
             onClose={props.onClose}
             onConfirm={props.onConfirm}>
-            <Markdown>{warning(card.name)}</Markdown>
+            <div className="flex flex-col">
+                <div className="pb-8">
+                    <span className="text-lg font-bold">
+                        <Text>{card.name}</Text>
+                    </span>
+                </div>
+                <div>
+                    <span className="text-xs text-gray-500">
+                        All data related to this card will be lost permanently.
+                    </span>
+                </div>
+            </div>
         </Dialog.Warning>
     );
 });
