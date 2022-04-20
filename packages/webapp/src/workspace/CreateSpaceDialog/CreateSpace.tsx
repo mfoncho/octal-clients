a31ab3d8631b9869f045c5d567ca6c4cecf53682
mfoncho <akonumboche@gmail.com>
2022-04-20 17:18:34 -0600

    update CreateSpaceDialog params to name, topic and access

--- packages/webapp/src/workspace/CreateSpaceDialog/CreateSpace.tsx
+++ packages/webapp/src/workspace/CreateSpaceDialog/CreateSpace.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 //import Switch from "@material-ui/core/Switch";
 import { useDispatch } from "react-redux";
-import { Dialog, Button, Switch } from "@octal/ui";
+import { Dialog, Button, Switch, Textarea } from "@octal/ui";
 import * as Icons from "@octal/icons";
 import { useInput } from "src/utils";
 import { useNavigator } from "src/hooks";
@@ -12,10 +12,12 @@ interface ISpaceCreator {
     onClose: () => void;
 }
 
-interface ISpaceType {
-    name: string;
-    type: "discuss" | "board";
-    description: string;
+interface IInput {
+    label: string;
+    value: string;
+    onChange: (value: string) => void;
+    subheader?: string;
+    placeholder?: string;
 }
 
 const accessDescription = {
@@ -24,23 +26,24 @@ const accessDescription = {
         "Space will be visible only to space members. Others can join the space by invition or adding them from the space workspace",
 };
 
-const spaceTypes: ISpaceType[] = [
-    {
-        name: "DISCUSSION",
-        type: "discuss",
-        description: "Space for ideas, discusion, and, chat",
-    },
-    {
-        name: "BOARD",
-        type: "board",
-        description: "Colaboration for projects and task",
-    },
-];
+function Input(props: IInput) {
+    return (
+        <div className="flex flex-col py-2">
+            <span className="font-semibold text-xs">{props.label}</span>
+            <Textarea.Input
+                value={props.value}
+                onChange={props.onChange}
+                placeholder={props.placeholder}
+                className="form-input rounded-md text-base"
+            />
+            <span className="text-xs text-gray-500">{props.subheader}</span>
+        </div>
+    );
+}
 
 export default React.memo<ISpaceCreator>((props) => {
     const dispatch = useDispatch();
 
-    const [type, setType] = useState<"board" | "discuss">("discuss");
     const [access, setAccess] = useState<"public" | "private">("public");
 
     const name = useInput("");
@@ -53,19 +56,10 @@ export default React.memo<ISpaceCreator>((props) => {
         event.preventDefault();
         event.stopPropagation();
         let payload: any = {
-            type: type,
-            access: access,
             name: name.value,
+            access: access,
+            topic: topic.value,
         };
-        if (type == "board") {
-            payload.board = {
-                name: topic.value,
-                columns: [],
-            };
-            payload.topics = [];
-        } else if (type == "discuss") {
-            payload.topics = [{ name: topic.value }];
-        }
         const action = createSpace(payload);
         dispatch(action).then(() => setCreating(false));
         setCreating(true);
@@ -87,69 +81,26 @@ export default React.memo<ISpaceCreator>((props) => {
             fullWidth={false}
             fullHeight={false}
             onClose={creating ? undefined : props.onClose}>
-            <Dialog.Content className="flex flex-col">
-                <div className="flex py-2 flex-col">
-                    {spaceTypes.map((stype) => (
-                        <button
-                            key={stype.type}
-                            onClick={() => setType(stype.type)}
-                            className="rounded-lg border-2 border-gray-200 mb-4 py-4 px-2 flex items-center flex-row hover:bg-primary-50">
-                            <div className="flex flex-grow flex-col items-start">
-                                <span className="font-bold text-base">
-                                    {stype.name}
-                                </span>
-                                <span className="text-sm">
-                                    {stype.description}
-                                </span>
-                            </div>
+            <Dialog.Content className="flex flex-col overflow-y-auto">
+                <Input
+                    label="Name"
+                    placeholder="name"
+                    value={name.value}
+                    onChange={name.setValue}
+                />
 
-                            <input
-                                type="radio"
-                                name="type"
-                                value={stype.type}
-                                onChange={() => {}}
-                                className="text-primary-500 rounded-full form-radio"
-                                checked={stype.type == type}
-                            />
-                        </button>
-                    ))}
-                </div>
-
-                <div className="flex flex-col py-2">
-                    <span className="font-semibold">SPACE NAME</span>
-                    <input
-                        required
-                        name="name"
-                        placeholder="name"
-                        className="form-input rounded-lg"
-                        {...name.props}
-                    />
-                </div>
-
-                <div className="flex flex-col py-2">
-                    <span className="font-semibold">
-                        {type == "board" && "BOARD NAME"}
-                        {type == "discuss" && "MAIN TOPIC"}
-                    </span>
-                    <input
-                        required
-                        name="topic"
-                        placeholder="topic"
-                        className="form-input rounded-md"
-                        {...topic.props}
-                    />
-                    {type == "board" && (
-                        <span className="text-xs">Board board name</span>
-                    )}
-                    {type == "discuss" && (
-                        <span className="text-xs">Main space topic</span>
-                    )}
-                </div>
+                <Input
+                    label="Topic"
+                    placeholder="topic"
+                    value={topic.value}
+                    onChange={topic.setValue}
+                    subheader="Main space topic"
+                />
 
                 <div className="flex flex-col py-4">
                     <div className="flex flex-row items-center">
                         <Icons.Private className="text-gray-500" />
-                        <span className="font-semibold px-2 flex-grow">
+                        <span className="font-semibold px-2 flex-grow text-xs">
                             Private
                         </span>
                         <Switch
@@ -157,7 +108,7 @@ export default React.memo<ISpaceCreator>((props) => {
                             onChange={toggleAccess}
                         />
                     </div>
-                    <span className="font-semibold text-xs text-gray-700">
+                    <span className="font-semibold text-xs text-gray-500 py-2">
                         {accessDescription[access]}
                     </span>
                 </div>
