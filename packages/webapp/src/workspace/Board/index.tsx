4266cf0b9537504e81b15f60a1b340ece9884642
mfoncho <akonumboche@gmail.com>
2022-05-10 13:51:18 -0600

    update load board columns and card on component mount

--- packages/webapp/src/workspace/Board/index.tsx
+++ packages/webapp/src/workspace/Board/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import Main from "./Main";
 import Board from "./Context";
-import { useBoardColumns, useBoardCards } from "@octal/store";
 import { useDispatch } from "react-redux";
 import * as BoardAction from "@octal/store/lib/actions/board";
 import { useBoard, useBoardLoaded } from "@octal/store";
@@ -9,28 +8,40 @@ import { useParams } from "react-router-dom";
 export * from "./hooks";
 
 export default React.memo(() => {
+    const [loading, setLoading] = React.useState<string[]>([]);
     const params = useParams<{ space_id: string; board_id: string }>();
     const dispatch = useDispatch();
     const board = useBoard(params.board_id!);
     const loaded = useBoardLoaded(params.board_id!);
 
-    const cards = useBoardCards(params.board_id!);
-
-    const columns = useBoardColumns(params.board_id!);
-
     useEffect(() => {
-        if (!loaded.includes("columns")) {
+        if (!Boolean(board)) {
+            return;
+        }
+        if (loaded.includes("columns")) {
+            if (loading.includes("columns")) {
+                setLoading((loading) =>
+                    loading.filter((col) => col !== "columns")
+                );
+            }
+        } else if (!loading.includes("columns")) {
             const action = BoardAction.loadBoardColumns(params as any);
             dispatch(action);
+            setLoading((loading) => loading.concat(["columns"]));
         }
-        if (!loaded.includes("cards")) {
+
+        if (loaded.includes("cards")) {
+            if (loading.includes("cards")) {
+                setLoading((loading) =>
+                    loading.filter((col) => col !== "cards")
+                );
+            }
+        } else if (!loading.includes("cards")) {
             const action = BoardAction.loadBoardCards(params as any);
             dispatch(action);
+            setLoading((loading) => loading.concat(["cards"]));
         }
-    }, [
-        cards ? cards.size == 0 : cards,
-        columns ? columns.size == 0 : columns,
-    ]);
+    }, [loaded]);
 
     if (board) {
         return (
