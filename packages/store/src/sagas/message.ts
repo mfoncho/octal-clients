d01407714591b96e84aab5d5171067768989270b
mfoncho <akonumboche@gmail.com>
2022-05-15 12:13:11 -0600

    update refactor message saga

--- packages/store/src/sagas/message.ts
+++ packages/store/src/sagas/message.ts
@@ -1,48 +1,15 @@
 import { put, select, takeEvery } from "redux-saga/effects";
 import { State } from "..";
-import {
-    FETCH_MESSAGES,
-    POST_MESSAGE,
-    UPDATE_MESSAGE,
-    NEW_MESSAGE,
-    FLAG_MESSAGE,
-    REACT_MESSAGE,
-    PIN_MESSAGE,
-    UNREACT_MESSAGE,
-    UNPIN_MESSAGE,
-    UNFLAG_MESSAGE,
-    DELETE_MESSAGE,
-    USER_REACTED,
-} from "../actions/types";
+import * as Actions from "../actions/types";
 import client from "@octal/client";
 import { MessageSchema, ReactionSchema } from "../schemas";
-import {
-    userReacted,
-    FetchMessagesAction,
-    PostMessageAction,
-    NewMessageAction,
-    newMessage,
-    concatConversation,
-    PinMessageAction,
-    FlagMessageAction,
-    UnflagMessageAction,
-    UnpinMessageAction,
-    UserReactedAction,
-    UserUnreactedAction,
-    messageUpdated,
-    MessageUpdatedAction,
-    UnreactMessageAction,
-    ReactMessageAction,
-    UpdateMessageAction,
-    DeleteMessageAction,
-    messageDeleted,
-    MessageDeletedAction,
-    userUnreacted,
-    reactionLoaded,
-} from "../actions/thread";
+import * as ThreadActions from "../actions/thread";
 import { relatedLoaded } from "../actions/app";
 
-function* fetch({ payload, resolve }: FetchMessagesAction): Iterable<any> {
+function* fetch({
+    payload,
+    resolve,
+}: ThreadActions.FetchMessagesAction): Iterable<any> {
     try {
         const data = (yield client.fetchMessages(payload)) as any;
         resolve.success(data);
@@ -51,21 +18,24 @@ function* fetch({ payload, resolve }: FetchMessagesAction): Iterable<any> {
     }
 }
 
-function* post({ payload, resolve }: PostMessageAction): Iterable<any> {
+function* post({
+    payload,
+    resolve,
+}: ThreadActions.PostMessageAction): Iterable<any> {
     try {
         const data = (yield client.postMessage(payload)) as any;
-        yield put(newMessage(data));
+        yield put(ThreadActions.newMessage(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* created({ payload }: NewMessageAction): Iterable<any> {
+function* created({ payload }: ThreadActions.NewMessageAction): Iterable<any> {
     const [normalized, related] = MessageSchema.normalizeOne(payload);
     yield put(relatedLoaded(related));
     yield put(
-        concatConversation({
+        ThreadActions.concatConversation({
             mode: "append",
             messages: [normalized],
             thread_id: normalized.thread_id!,
@@ -73,57 +43,75 @@ function* created({ payload }: NewMessageAction): Iterable<any> {
     );
 }
 
-function* flag({ payload, resolve }: FlagMessageAction): Iterable<any> {
+function* flag({
+    payload,
+    resolve,
+}: ThreadActions.FlagMessageAction): Iterable<any> {
     try {
         const data = (yield client.flagMessage(payload)) as any;
-        yield put(messageUpdated(data));
+        yield put(ThreadActions.messageUpdated(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* pin({ payload, resolve }: PinMessageAction): Iterable<any> {
+function* pin({
+    payload,
+    resolve,
+}: ThreadActions.PinMessageAction): Iterable<any> {
     try {
         const data = (yield client.pinMessage(payload)) as any;
-        yield put(messageUpdated(data));
+        yield put(ThreadActions.messageUpdated(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* unflag({ payload, resolve }: UnflagMessageAction): Iterable<any> {
+function* unflag({
+    payload,
+    resolve,
+}: ThreadActions.UnflagMessageAction): Iterable<any> {
     try {
         const data = (yield client.unflagMessage(payload)) as any;
-        yield put(messageUpdated(data));
+        yield put(ThreadActions.messageUpdated(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* unpin({ payload, resolve }: UnpinMessageAction): Iterable<any> {
+function* unpin({
+    payload,
+    resolve,
+}: ThreadActions.UnpinMessageAction): Iterable<any> {
     try {
         const data = (yield client.unpinMessage(payload)) as any;
-        yield put(messageUpdated(data));
+        yield put(ThreadActions.messageUpdated(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* react({ payload, resolve }: ReactMessageAction): Iterable<any> {
+function* react({
+    payload,
+    resolve,
+}: ThreadActions.ReactMessageAction): Iterable<any> {
     try {
         const data = (yield client.reactMessage(payload)) as any;
-        yield put(userReacted(data));
+        yield put(ThreadActions.userReacted(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* unreact({ payload, resolve }: UnreactMessageAction): Iterable<any> {
+function* unreact({
+    payload,
+    resolve,
+}: ThreadActions.UnreactMessageAction): Iterable<any> {
     try {
         const data = (yield client.unreactMessage(payload)) as any;
         const { auth } = (yield select()) as any as State;
@@ -133,18 +121,21 @@ function* unreact({ payload, resolve }: UnreactMessageAction): Iterable<any> {
             thread_id: payload.thread_id,
             message_id: payload.message_id,
         };
-        yield put(userUnreacted(partial as any));
+        yield put(ThreadActions.userUnreacted(partial as any));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* trash({ payload, resolve }: DeleteMessageAction): Iterable<any> {
+function* trash({
+    payload,
+    resolve,
+}: ThreadActions.DeleteMessageAction): Iterable<any> {
     try {
         const data = (yield client.deleteMessage(payload)) as any;
         yield put(
-            messageDeleted({
+            ThreadActions.messageDeleted({
                 id: payload.message_id,
                 thread_id: payload.thread_id,
             })
@@ -155,33 +146,36 @@ function* trash({ payload, resolve }: DeleteMessageAction): Iterable<any> {
     }
 }
 
-function* update({ payload, resolve }: UpdateMessageAction): Iterable<any> {
+function* update({
+    payload,
+    resolve,
+}: ThreadActions.UpdateMessageAction): Iterable<any> {
     try {
         const data = (yield client.updateMessage(payload)) as any;
-        yield put(messageUpdated(data));
+        yield put(ThreadActions.messageUpdated(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* reacted({ payload }: UserReactedAction): Iterable<any> {
+function* reacted({ payload }: ThreadActions.UserReactedAction): Iterable<any> {
     const [normalized, related] = ReactionSchema.normalizeOne(payload);
     yield put(relatedLoaded(related));
-    yield put(reactionLoaded(normalized as any));
+    yield put(ThreadActions.reactionLoaded(normalized as any));
 }
 
 export const tasks = [
-    { effect: takeEvery, type: USER_REACTED, handler: reacted },
-    { effect: takeEvery, type: FETCH_MESSAGES, handler: fetch },
-    { effect: takeEvery, type: POST_MESSAGE, handler: post },
-    { effect: takeEvery, type: NEW_MESSAGE, handler: created },
-    { effect: takeEvery, type: PIN_MESSAGE, handler: pin },
-    { effect: takeEvery, type: FLAG_MESSAGE, handler: flag },
-    { effect: takeEvery, type: UNPIN_MESSAGE, handler: unpin },
-    { effect: takeEvery, type: UNFLAG_MESSAGE, handler: unflag },
-    { effect: takeEvery, type: REACT_MESSAGE, handler: react },
-    { effect: takeEvery, type: UNREACT_MESSAGE, handler: unreact },
-    { effect: takeEvery, type: UPDATE_MESSAGE, handler: update },
-    { effect: takeEvery, type: DELETE_MESSAGE, handler: trash },
+    { effect: takeEvery, type: Actions.USER_REACTED, handler: reacted },
+    { effect: takeEvery, type: Actions.FETCH_MESSAGES, handler: fetch },
+    { effect: takeEvery, type: Actions.POST_MESSAGE, handler: post },
+    { effect: takeEvery, type: Actions.NEW_MESSAGE, handler: created },
+    { effect: takeEvery, type: Actions.PIN_MESSAGE, handler: pin },
+    { effect: takeEvery, type: Actions.FLAG_MESSAGE, handler: flag },
+    { effect: takeEvery, type: Actions.UNPIN_MESSAGE, handler: unpin },
+    { effect: takeEvery, type: Actions.UNFLAG_MESSAGE, handler: unflag },
+    { effect: takeEvery, type: Actions.REACT_MESSAGE, handler: react },
+    { effect: takeEvery, type: Actions.UNREACT_MESSAGE, handler: unreact },
+    { effect: takeEvery, type: Actions.UPDATE_MESSAGE, handler: update },
+    { effect: takeEvery, type: Actions.DELETE_MESSAGE, handler: trash },
 ];
