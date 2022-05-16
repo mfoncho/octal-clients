e141c382e976c96e4e577c6358fc3c41852dba4a
mfoncho <akonumboche@gmail.com>
2022-05-15 22:40:31 -0600

    update navigator background color to slate

--- packages/webapp/src/workspace/Navigator/index.tsx
+++ packages/webapp/src/workspace/Navigator/index.tsx
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Sections from "@workspace/Sections";
 import CreateSpaceBtn from "./CreateSpaceBtn";
 import { Scrollbars } from "react-custom-scrollbars";
 import { SpaceRecord, useSpaces } from "@octal/store";
 import Space from "./Space";
-
-const search = (
-    <div className="flex flex-col px-2 py-2 overflow-hidden">
-        <input className="focus:outline-none focus:ring focus:border-blue-300  rounded-md border border-gray-200 py-1 px-2 w-full text-base text-gray-800" />
-    </div>
-);
 
 export default React.memo(() => {
     let spaces = useSpaces();
@@ -20,7 +14,6 @@ export default React.memo(() => {
 
     return (
         <div className="flex flex-grow flex-col overflow-hidden">
-            {search}
             <Scrollbars autoHide className="flex flex-col">
                 <div className="flex flex-col px-2 pb-14 overflow-hidden">
                     <div className="py-2">
