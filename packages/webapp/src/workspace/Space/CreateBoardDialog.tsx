ae9afa12e582e55b0b6333dcc6f02f400d08e20b
mfoncho <akonumboche@gmail.com>
2022-05-16 12:42:32 -0600

    update create board dialog use Textarea.Input component

--- packages/webapp/src/workspace/Space/CreateBoardDialog.tsx
+++ packages/webapp/src/workspace/Space/CreateBoardDialog.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import * as Icons from "@octal/icons";
-import { Dialog, Text, Emoji, Button } from "@octal/ui";
+import { Dialog, Text, Textarea, Button } from "@octal/ui";
 import { SpaceRecord } from "@octal/store/lib/records";
 import { useNavigator } from "src/hooks";
 import { useInput } from "src/utils";
@@ -14,12 +14,7 @@ export default Dialog.create<IDialog>((props) => {
     const actions = useActions(props.space);
     const navigator = useNavigator();
     const [loading, setLoading] = useState(false);
-    const iconBtnRef = useRef<HTMLButtonElement>(null);
-    const [picker, setPicker] = useState<boolean>(false);
-
     const name = useInput("");
-
-    const icon = useInput("");
 
     function handleCreateTopic(e: React.MouseEvent) {
         e.stopPropagation();
@@ -27,10 +22,7 @@ export default Dialog.create<IDialog>((props) => {
         setLoading(true);
         actions
             .createBoard({
-                name: (icon.valid
-                    ? `${icon.value} ${name.value}`
-                    : name.value
-                ).trim(),
+                name: name.value,
             })
             .then((board) => {
                 navigator.openBoard(board);
@@ -40,14 +32,6 @@ export default Dialog.create<IDialog>((props) => {
                 setLoading(false);
             });
     }
-
-    function handleKeyPress(e: React.KeyboardEvent) {
-        if (e.key == "Backspace" && (e.target as any).selectionStart == 0) {
-            icon.setValue("");
-        }
-    }
-
-    const namePreview = `${icon.value} ${name.value}`.trim();
 
     return (
         <Dialog.Base
@@ -62,8 +46,8 @@ export default Dialog.create<IDialog>((props) => {
                     <Icons.Board className="h-6 w-6 text-primary-500" />
                     <span className="font-extrabold text-xl px-2 text-gray-800">
                         <Text>
-                            {namePreview.length > 0
-                                ? namePreview
+                            {name.value.length > 0
+                                ? name.value
                                 : "Create Board"}
                         </Text>
                     </span>
@@ -75,37 +59,14 @@ export default Dialog.create<IDialog>((props) => {
                 </button>
             </div>
             <Dialog.Content className="flex flex-col">
-                <span className="py-1 font-semibold text-gray-800 text-xs">
-                    BOARD NAME
-                </span>
                 <div className="relative flex flex-row">
-                    <input
+                    <Textarea.Input
                         disabled={loading}
-                        className="form-input font-semibold w-full text-gray-800 pl-11 rounded-md border-gray-400"
-                        {...name.props}
-                        onKeyUp={handleKeyPress}
+                        className="form-input focus:ring text-base font-semibold w-full text-gray-800 rounded-md border-gray-400"
+                        value={name.value}
+                        onChange={name.setValue}
                     />
-                    <div className="absolute h-full py-2 px-1.5 flex items-center justify-center">
-                        <Button
-                            ref={iconBtnRef}
-                            disabled={loading}
-                            className="text-lg"
-                            onClick={(e) => setPicker(true)}
-                            variant="icon">
-                            <Text>{icon.value}</Text>
-                        </Button>
-                    </div>
                 </div>
-                <Emoji.Picker.Popper
-                    open={picker}
-                    anchorEl={iconBtnRef.current}
-                    placement="top-start"
-                    onSelect={(val) => {
-                        icon.setValue(val);
-                        setPicker(false);
-                    }}
-                    onClickAway={() => setPicker(false)}
-                />
             </Dialog.Content>
             <Dialog.Actions className="rounded-b-lg">
                 <Button
