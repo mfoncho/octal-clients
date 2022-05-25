205980df4f02e7f5e5b7025b3fb12738ef4905ad
mfoncho <akonumboche@gmail.com>
2022-05-24 21:23:25 -0600

    update track inialized board and navigate to space default topic on topic delete

--- packages/webapp/src/workspace/Topic/index.tsx
+++ packages/webapp/src/workspace/Topic/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "./Header";
 import Thread from "../Thread";
+import { useNavigator } from "src/hooks";
 import { useTopic } from "@octal/store";
 
 export default React.memo(() => {
-    const params = useParams<{ topic_id: string }>();
+    const nav = useNavigator();
+    const [init, setInit] = useState<boolean>(false);
+    const params = useParams<{ topic_id: string; space_id: string }>();
     const topic = useTopic(params.topic_id!);
+
+    useEffect(() => {
+        if (!init && !Boolean(topic.id)) {
+            setInit(true);
+        }
+        if (init && topic.id !== params.topic_id) {
+            nav.openSpace({ id: params.space_id! });
+        }
+    }, [topic.id, params.topic_id]);
+
     return (
         <div className="flex flex-grow flex-col">
             <Header />
