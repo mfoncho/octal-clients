287fcda2f90d6838fade68000faa9c81ced6ad61
mfoncho <akonumboche@gmail.com>
2022-05-26 01:16:08 -0600

    add UserAvatar Img component

---
+++ packages/webapp/src/workspace/UserAvatar/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Tooltip } from "@octal/ui";
+import { useUser } from "@octal/store";
+
+interface IUserAvatar {
+    uid: string;
+    tooltip?: boolean;
+    className?: string;
+}
+
+export default React.memo<IUserAvatar>((props) => {
+    const user = useUser(props.uid);
+    const image = (
+        <img
+            className={props.className}
+            alt={user.username}
+            src={user.avatar}
+        />
+    );
+    if (props.tooltip) {
+        const title = (
+            <span className="font-semibold text-sm">{user.username}</span>
+        );
+
+        return (
+            <Tooltip placement="top" title={title}>
+                {image}
+            </Tooltip>
+        );
+    }
+    return image;
+});
