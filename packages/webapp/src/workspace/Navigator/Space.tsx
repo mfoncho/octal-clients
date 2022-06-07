a6e9d83897bf7f043630444ef53113f821eac86e
mfoncho <akonumboche@gmail.com>
2022-06-06 23:48:37 -0600

    update added invitation to nav space nav menu

--- packages/webapp/src/workspace/Navigator/Space.tsx
+++ packages/webapp/src/workspace/Navigator/Space.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import clx from "classnames";
 import { Link, generatePath, useParams } from "react-router-dom";
 import { Text, Collapse, Dialog, Popper } from "@octal/ui";
@@ -10,10 +10,12 @@ import Topic from "./Topic";
 import Board from "./Board";
 import { BsThreeDots as MenuIcon } from "react-icons/bs";
 import SpaceSettingsDialog from "../SpaceSettingsDialog";
+import InviteDialog from "../InviteDialog";
 import {
     SpaceRecord,
     useUser,
     useAuthId,
+    useSpacePermissions,
     useSpaceBoardsIndex,
     useSpaceTopicsIndex,
 } from "@octal/store";
@@ -22,24 +24,15 @@ export interface ISpace {
     space: SpaceRecord;
 }
 
-export interface IMenu {
-    onSelect: (e: React.MouseEvent) => void;
+export interface IMenuItem {
+    name: string;
+    menu: string;
 }
 
-const options = [
-    {
-        menu: "topic",
-        name: "Topic",
-    },
-    {
-        menu: "board",
-        name: "Board",
-    },
-    {
-        menu: "settings",
-        name: "Settings",
-    },
-];
+export interface IMenu {
+    onSelect: (e: React.MouseEvent) => void;
+    options: IMenuItem[];
+}
 
 const Menu = Popper.create<HTMLUListElement, IMenu>((props) => {
     return (
@@ -52,7 +45,7 @@ const Menu = Popper.create<HTMLUListElement, IMenu>((props) => {
             placement="bottom-start"
             onClickAway={props.onClickAway}
             className="focus:outline-none flex w-56 flex-col rounded-md ring-1 ring-gray-800 ring-opacity-5 max-h-52 p-2 bg-white shadow-md overflow-x-hidden">
-            {options.map((item, index) => (
+            {props.options.map((item, index) => (
                 <li
                     key={String(index)}
                     data-menu-name={item.menu}
@@ -111,9 +104,15 @@ export const DirectSpace = React.memo<ISpace>(({ space }) => {
 export const GeneralSpace = React.memo<ISpace>(({ space }) => {
     const dialog = Dialog.useDialog();
 
+    const authid = useAuthId();
+
     const menuRef = React.useRef<HTMLButtonElement | null>(null);
 
     const params = useParams<{ space_id: string }>();
+
+    const permissions = useSpacePermissions(space.id);
+
+    const [menu, setMenu] = useState<IMenuItem[]>([]);
 
     const boards = useSpaceBoardsIndex(space.id);
 
@@ -124,6 +123,37 @@ export const GeneralSpace = React.memo<ISpace>(({ space }) => {
     const [hovering, setHovering] = useState<boolean>(false);
 
     const highlight = params.space_id == space.id && !expaned;
+
+    useEffect(() => {
+        let menu: IMenuItem[] = [];
+        if (permissions.get("space.manage")) {
+            menu.push({
+                menu: "board",
+                name: "Board",
+            });
+        }
+        if (permissions.get("space.manage")) {
+            menu.push({
+                menu: "topic",
+                name: "Topic",
+            });
+        }
+
+        if (permissions.get("invitation.send")) {
+            menu.push({
+                menu: "invite",
+                name: "Invite",
+            });
+        }
+
+        if (permissions.get("space.manage") || authid === space.admin_id) {
+            menu.push({
+                menu: "settings",
+                name: "Settings",
+            });
+        }
+        setMenu(menu);
+    }, [permissions]);
 
     function handleToggleExpanded(e: React.MouseEvent) {
         e.stopPropagation();
@@ -171,41 +201,59 @@ export const GeneralSpace = React.memo<ISpace>(({ space }) => {
                     )}>
                     <Text>{space.name.toUpperCase()}</Text>
                 </span>
-                <button
-                    ref={menuRef}
-                    className={clx(
-                        "flex justify-center items-center p-1 bg-gray-200 text-gray-500 rounded-md w-6 h-6",
-                        !hovering && "invisible"
-                    )}
-                    onClick={dialog.opener("menu")}>
-                    <MenuIcon />
-                </button>
+                {menu.length > 0 && (
+                    <button
+                        ref={menuRef}
+                        className={clx(
+                            "flex justify-center items-center p-1 bg-gray-200 text-gray-500 rounded-md w-6 h-6",
+                            !hovering && "invisible"
+                        )}
+                        onClick={dialog.opener("menu")}>
+                        <MenuIcon />
+                    </button>
+                )}
             </div>
-            <Menu
-                open={dialog.menu}
-                onSelect={handleOpenMenu}
-                anchorEl={menuRef.current}
-                onClickAway={dialog.close}
-            />
+            {menu.length > 0 && (
+                <Menu
+                    options={menu}
+                    open={dialog.menu}
+                    onSelect={handleOpenMenu}
+                    anchorEl={menuRef.current}
+                    onClickAway={dialog.close}
+                />
+            )}
             <Collapse unmountOnExit={false} in={expaned}>
                 {renderBoards()}
                 {renderTopics()}
             </Collapse>
-            <SpaceSettingsDialog
-                space={space}
-                open={dialog.settings}
-                onClose={dialog.close}
-            />
-            <CreateBoardDialog
-                space={space}
-                open={dialog.board}
-                onClose={dialog.close}
-            />
-            <TopicCreatorDialog
-                space={space}
-                open={dialog.topic}
-                onClose={dialog.close}
-            />
+            {permissions.get("space.manage") && (
+                <React.Fragment>
+                    <CreateBoardDialog
+                        space={space}
+                        open={dialog.board}
+                        onClose={dialog.close}
+                    />
+                    <TopicCreatorDialog
+                        space={space}
+                        open={dialog.topic}
+                        onClose={dialog.close}
+                    />
+                </React.Fragment>
+            )}
+            {(permissions.get("space.manage") || authid === space.admin_id) && (
+                <SpaceSettingsDialog
+                    space={space}
+                    open={dialog.settings}
+                    onClose={dialog.close}
+                />
+            )}
+            {permissions.get("invitation.send") && (
+                <InviteDialog
+                    open={dialog.invite}
+                    space={space}
+                    onClose={dialog.close}
+                />
+            )}
         </React.Fragment>
     );
 });
