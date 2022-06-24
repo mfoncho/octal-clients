1f045bccac379d49a582a2ed96713b2629a126a0
mfoncho <akonumboche@gmail.com>
2022-06-24 05:31:55 -0600

    update home checklist link to card

--- packages/webapp/src/workspace/Home/Assigned.tsx
+++ packages/webapp/src/workspace/Home/Assigned.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import { List } from "immutable";
-import {
-    BiCheckboxChecked as DoneIcon,
-    BiCheckbox as UndoneIcon,
-} from "react-icons/bi";
-import CardDialog from "@workspace/CardDialog";
+import * as Icons from "@octal/icons";
 import { sort } from "@octal/common";
 import { Text, Tooltip } from "@octal/ui";
+import paths from "src/paths";
+import { Link, generatePath } from "react-router-dom";
 import {
     CardTaskValueRecord,
     useUserChecklists,
@@ -56,35 +54,27 @@ export const UserAvatar = React.memo<IUserAvatar>(({ users }) => {
 });
 
 function Checklist(props: ICard) {
-    const [dialog, setDialog] = useState(false);
-
-    function handleOpenCard(e: React.MouseEvent) {
-        e.stopPropagation();
-        e.preventDefault();
-        setDialog(true);
-    }
-
-    function handleCloseCard() {
-        setDialog(false);
-    }
+    const cardPath = generatePath(paths.workspace.card, {
+        board_id: props.board.id,
+        space_id: props.board.space_id,
+        card_id: props.card.id,
+    });
 
     const subheader = (
-        <div className="inline-flex flex-row">
+        <Link to={cardPath} className="inline-flex flex-row">
             <span className="text-sm font-semibold text-gray-500">
                 <Text>{props.card.name}</Text>
             </span>
             <span className="text-xs px-2 text-gray-500">
                 (<Text>{props.board.name}</Text>)
             </span>
-        </div>
+        </Link>
     );
+
     return (
         <div className="flex flex-col rounded-lg bg-white p-4">
             <div className="flex flex-row justify-between">
-                <span
-                    onClick={handleOpenCard}
-                    role="button"
-                    className="cursor-pointer font-bold">
+                <span className="cursor-pointer font-bold">
                     <Text>{props.name}</Text>
                 </span>
                 <UserAvatar users={props.users} />
@@ -94,25 +84,17 @@ function Checklist(props: ICard) {
                 {props.tasks.map((task) => (
                     <div key={task.id} className="flex flex-row items-center">
                         {task.done ? (
-                            <DoneIcon className="h-6 w-6 text-primary-500" />
+                            <Icons.Task.DoneSolid className="h-5 w-5 text-primary-500" />
                         ) : (
-                            <UndoneIcon className="h-6 w-6 text-gray-500" />
+                            <Icons.Task.Undone className="h-5 w-5 text-gray-500" />
                         )}
 
-                        <span
-                            onClick={handleOpenCard}
-                            role="button"
-                            className="cursor-pointer px-2 font-semibold text-sm text-gray-500">
+                        <span className="cursor-pointer px-2 font-semibold text-sm text-gray-500">
                             <Text>{task.name}</Text>
                         </span>
                     </div>
                 ))}
             </div>
-            <CardDialog
-                open={dialog}
-                id={props.card.id}
-                onClose={handleCloseCard}
-            />
         </div>
     );
 }
