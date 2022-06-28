ffddb9b6a388375ebed2905fc56bbd9f151f032b
mfoncho <akonumboche@gmail.com>
2022-06-27 22:00:17 -0600

    update FileRecord

--- packages/store/src/records/workspace.ts
+++ packages/store/src/records/workspace.ts
@@ -6,7 +6,24 @@ export class FileRecord extends Record({
     ext: "",
     name: "",
     size: 0,
+    bucket: "",
     preview: "",
     filename: "",
-    bucket_id: "",
-}) {}
+}) {
+    get humanSize() {
+        let decimals = 2;
+        if (this.size === 0) return "0 Bytes";
+
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+        const i = Math.floor(Math.log(this.size) / Math.log(k));
+
+        return (
+            parseFloat((this.size / Math.pow(k, i)).toFixed(dm)) +
+            " " +
+            sizes[i]
+        );
+    }
+}
