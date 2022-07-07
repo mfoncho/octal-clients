af0bf048674f631b69c2d76cf82af1f2887d613a
mfoncho <akonumboche@gmail.com>
2022-07-07 01:39:53 -0600

    update submit post form

--- packages/webapp/src/workspace/Message/Editor.tsx
+++ packages/webapp/src/workspace/Message/Editor.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Textarea } from "@octal/ui";
-import { Dialog, UIEvent } from "@octal/ui";
-import { useMessage } from "@octal/store";
+import { Dialog } from "@octal/ui";
+import { useMessage, MessageRecord } from "@octal/store";
+import { useInput } from "src/utils";
 import { updateMessage } from "@octal/store/lib/actions/thread";
 
 export interface IEditor {
@@ -10,44 +11,57 @@ export interface IEditor {
     onClose: () => void;
 }
 
-export default React.memo<IEditor>((props: any) => {
-    const dispatch = useDispatch();
-    const message = useMessage(props.id);
+const Editor = React.memo<{ message: MessageRecord; onClose: () => void }>(
+    ({ message, onClose }) => {
+        const dispatch = useDispatch();
+        const content = useInput(message.content ?? "");
+        const [loading, setLoading] = useState<boolean>(false);
 
-    const [loading, setLoading] = useState<boolean>(false);
+        function handleSavePost(e: any) {
+            let value = e.target.value.trim();
+            if (
+                loading == false &&
+                value.length > 1 &&
+                value !== message.content
+            ) {
+                const params = {
+                    params: {
+                        content: value,
+                    },
+                    thread_id: message!.thread_id,
+                    message_id: message!.id,
+                    space_id: message!.space_id,
+                };
 
-    function handleSavePost(e: any) {
-        if (e.target.value.trim().length < 1) {
-            const params = {
-                params: {
-                    content: e.target.value.trim(),
-                },
-                thread_id: message!.thread_id,
-                message_id: message!.id,
-                space_id: message!.space_id,
-            };
-
-            setLoading(true);
-            return dispatch(updateMessage(params))
-                .then(props.onClose)
-                .catch(() => {
-                    setLoading(false);
-                });
+                setLoading(true);
+                return dispatch(updateMessage(params))
+                    .then(onClose)
+                    .catch(() => {
+                        setLoading(false);
+                    });
+            }
         }
+        return (
+            <Textarea.Post
+                {...content.props}
+                autoFocus={true}
+                disabled={loading}
+                onSubmit={handleSavePost}
+            />
+        );
     }
+);
+
+export default React.memo<IEditor>((props: any) => {
+    const message = useMessage(props.id)!;
 
     return (
         <Dialog.Base
             open={Boolean(message)}
             fullWidth
             maxWidth="sm"
-            onClose={loading ? undefined : props.onClose}>
-            <Textarea.Post
-                value={message!.content}
-                autoFocus={true}
-                disabled={loading}
-                onSubmit={handleSavePost}
-            />
+            onClose={props.onClose}>
+            <Editor message={message} onClose={props.onClose} />
         </Dialog.Base>
     );
 });
