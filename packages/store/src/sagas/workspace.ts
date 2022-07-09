32c764e0da0b954eaafba1bccba8ae5fa6eb038e
mfoncho <akonumboche@gmail.com>
2022-07-09 07:25:45 -0600

    update dispatch workspace connection on auth with user

--- packages/store/src/sagas/workspace.ts
+++ packages/store/src/sagas/workspace.ts
@@ -1,32 +1,35 @@
 import { put, takeEvery } from "redux-saga/effects";
 import client from "@octal/client";
-import * as Actions from "../actions/app";
+import * as Actions from "../actions/types";
+import * as AppActions from "../actions/app";
 
-function* load({ resolve }: Actions.LoadWorkspaceAction): Iterable<any> {
+function* load({ resolve }: AppActions.LoadWorkspaceAction): Iterable<any> {
     try {
         const data = (yield client.getWorkspace()) as any;
-        yield put(Actions.workspaceLoaded(data));
+        yield put(AppActions.workspaceLoaded(data));
         resolve.success(data);
     } catch (e) {
         resolve.error(e);
     }
 }
 
-function* connect(_action: Actions.WorkspaceLoadedAction): Iterable<any> {
-    const topic = `workspace`;
+function* connect({ payload }: AppActions.AuthAction): Iterable<any> {
+    if (payload.user) {
+        const topic = `workspace`;
 
-    if (client.topic(topic)) return;
+        if (client.topic(topic)) return;
 
-    let channel = client.channel(topic);
+        let channel = client.channel(topic);
 
-    channel
-        .subscribe()
-        .receive("ok", () => {})
-        .receive("error", () => {});
-    yield put(Actions.workspaceConnected(channel, topic));
+        channel
+            .subscribe()
+            .receive("ok", () => {})
+            .receive("error", () => {});
+        yield put(AppActions.workspaceConnected(channel, topic));
+    }
 }
 
 export const tasks = [
     { effect: takeEvery, type: Actions.LOAD_WORKSPACE, handler: load },
-    { effect: takeEvery, type: Actions.WORKSPACE_LOADED, handler: connect },
+    { effect: takeEvery, type: Actions.AUTH_LOADED, handler: connect },
 ];
