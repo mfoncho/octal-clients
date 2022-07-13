8c9cee7c86b7d7ea1b359c7b1ae957e4dadff8d2
mfoncho <akonumboche@gmail.com>
2022-07-13 06:53:43 -0600

    update account theme selector ui

--- packages/webapp/src/workspace/AccountDialog/Preferences/ThemePreference.tsx
+++ packages/webapp/src/workspace/AccountDialog/Preferences/ThemePreference.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { colors } from "src/theme";
-import { noop, IPreference } from "./hooks";
+import * as Icons from "@octal/icons";
+import { IPreference } from "./hooks";
 
 const preference = "webapp.theme.color";
 
@@ -18,22 +19,29 @@ export default React.memo<IPreference>((props) => {
         let palette = colors[key as any];
         if (typeof palette === "object") {
             return (
-                <input
+                <div
                     key={key}
-                    onChange={noop}
-                    checked={props.preferences.get(preference) == key}
-                    onClick={handleSetTheme(key)}
-                    type="checkbox"
-                    className="form-checkbox border-transparent m-2 w-8 h-8 rounded-full"
-                    style={{
-                        backgroundColor: palette["700"],
-                    }}
-                />
+                    className="flex flex-row min-w-[10rem] h-32 rounded-md shadow overflow-hidden border border-slate-300">
+                    <div
+                        className="w-1/4"
+                        style={{
+                            backgroundColor: palette["700"],
+                        }}></div>
+                    <div className="flex-1 bg-slate-200 flex-row p-3 flex justify-end">
+                        <button
+                            onClick={handleSetTheme(key)}
+                            className="flex bg-white w-6 h-6 rounded-full justify-center items-center">
+                            {props.preferences.get(preference) == key && (
+                                <Icons.CheckedDot className="w-6 h-6 text-primary-700" />
+                            )}
+                        </button>
+                    </div>
+                </div>
             );
         }
     }
     return (
-        <div className="flex flex-row flex-wrap py-8">
+        <div className="flex flex-row py-8 space-x-4 overflow-y-auto">
             {Object.keys(colors).map(renderColor)}
         </div>
     );
