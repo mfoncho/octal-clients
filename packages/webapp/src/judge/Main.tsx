3dfb5d681ed1cc44b133069f247978c9883b463b
mfoncho <akonumboche@gmail.com>
2022-07-21 04:39:48 -0600

    use only store authid as loggin detector

--- packages/webapp/src/judge/Main.tsx
+++ packages/webapp/src/judge/Main.tsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useAuthId } from "@octal/store";
 import { useDispatch } from "react-redux";
-import { Actions } from "@octal/store"
-import client from "@octal/client";
+import { Actions } from "@octal/store";
 import Auth from "../auth";
 import App from "../workspace";
 
 export default React.memo(() => {
     const id = useAuthId();
     const dispatch = useDispatch();
-    const [authenticated, setAuthenticated] = useState<boolean | null>(null);
 
     useEffect(() => {
         dispatch(Actions.App.loadConfig());
@@ -17,34 +15,12 @@ export default React.memo(() => {
     }, []);
 
     useEffect(() => {
-        if (authenticated === null && !Boolean(id)) {
-            dispatch(Actions.App.loadAuth()).then((auth) => {
-                if (auth.token && auth.user) {
-                    setAuthenticated(true);
-                } else {
-                    setAuthenticated(false);
-                }
-            });
-        } else {
-            if (Boolean(id) && !authenticated) {
-                setAuthenticated(true);
-            } else if (!Boolean(id.trim()) && authenticated) {
-                setAuthenticated(false);
-            }
+        if (Boolean(id)) {
+            dispatch(Actions.App.loadAuth());
         }
     }, [id]);
 
-    useEffect(() => {
-        if (Boolean(id.trim()) && authenticated === false) {
-            client.shutdown();
-        }
-    }, [id, authenticated]);
-
-    if (authenticated === null) {
-        return <div />;
-    }
-
-    if (authenticated) {
+    if (Boolean(id)) {
         return <App />;
     }
 
