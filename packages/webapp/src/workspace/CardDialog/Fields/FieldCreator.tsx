0fa59ab5865ea366e77d59d4594153c5baf75f2e
mfoncho <akonumboche@gmail.com>
2022-07-21 21:46:00 -0600

    update field creator ui

--- packages/webapp/src/workspace/CardDialog/Fields/FieldCreator.tsx
+++ packages/webapp/src/workspace/CardDialog/Fields/FieldCreator.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Popper, Dialog } from "@octal/ui";
+import * as Icons from "@octal/icons";
+import { Popper, Dialog, Button } from "@octal/ui";
 import { useActions } from "../hooks";
-import { MdOutlineAddCircleOutline as AddIcon } from "react-icons/md";
 import { CardRecord } from "@octal/store";
 
 interface IOption {
@@ -19,26 +19,32 @@ const options: IOption[] = [
     {
         name: "Text",
         type: "text",
+        icon: Icons.Field.Text,
     },
     {
         name: "User",
         type: "user",
+        icon: Icons.Field.Users,
     },
     {
         name: "Label",
         type: "label",
+        icon: Icons.Field.Label,
     },
     {
-        name: "Date & Time",
+        name: "Date",
         type: "datetime",
+        icon: Icons.Field.DateTime,
     },
     {
         name: "Checklist",
         type: "checklist",
+        icon: Icons.Field.Checklist,
     },
     {
         name: "Files",
         type: "file",
+        icon: Icons.Field.File,
     },
 ];
 
@@ -51,13 +57,16 @@ const FieldsPopper = Popper.create<HTMLDivElement, IPopper>((props) => {
             anchorEl={props.anchorEl}
             placement="bottom-start"
             onClickAway={props.onClickAway}
-            className="focus:outline-none flex flex-col rounded-md ring-1 ring-gray-800 ring-opacity-5 min-w-[200px] p-2 bg-white shadow-md">
+            className="focus:outline-none flex flex-col rounded-md ring-1 ring-gray-800 ring-opacity-5 min-w-[180px] bg-white shadow-md overflow-hidden divide-y">
             {options.map((opt) => (
                 <button
                     onClick={(e) => props.onSelect(e, opt)}
                     key={opt.name}
-                    className="flex hover:bg-primary-500 hover:text-white flex-row justify-between px-2 py-1 rounded-md">
-                    <span className="font-semibold text-sm">{opt.name}</span>
+                    className="flex hover:bg-primary-500 hover:text-white flex-row justify-between px-3 py-2 text-gray-700 items-center">
+                    <span className="font-semibold text-xs uppercase">
+                        {opt.name}
+                    </span>
+                    {React.createElement(opt.icon, { className: "w-4 h-4" })}
                 </button>
             ))}
         </Popper>
@@ -76,13 +85,14 @@ export default React.memo<{ card: CardRecord }>((props) => {
     }
 
     return (
-        <div className="flex py-4 flex-row flex-wrap">
-            <button
+        <div className="flex py-4 px-2 flex-row flex-wrap">
+            <Button
                 ref={btnRef}
-                onClick={dialog.opener("popper")}
-                className="font-bold text-gray-600">
-                <AddIcon className="w-6 h-6" />
-            </button>
+                variant="icon"
+                color="primary"
+                onClick={dialog.opener("popper")}>
+                <Icons.Plus className="w-6 h-6" />
+            </Button>
             <FieldsPopper
                 open={dialog.popper}
                 onSelect={handleMenuSelect}
