72da10cbc990ff166ecdb8d37ad97772c18532ce
mfoncho <akonumboche@gmail.com>
2022-07-23 08:13:24 -0600

    remove dispatch counter load topic thread

--- packages/webapp/src/workspace/Navigator/Counter.tsx
+++ packages/webapp/src/workspace/Navigator/Counter.tsx
@@ -1,23 +1,8 @@
 import React from "react";
 import clx from "classnames";
 import { useUnreadCount } from "@octal/store";
-import { useDispatch } from "react-redux";
-import { useThread } from "@octal/store";
-import * as ThreadActionFactory from "@octal/store/lib/actions/thread";
-
 export default React.memo<{ id: string }>((props) => {
-    const dispatch = useDispatch();
-    const thread = useThread(props.id);
     let count = useUnreadCount(props.id);
-    React.useEffect(() => {
-        if (!Boolean(thread)) {
-            const action = ThreadActionFactory.loadThread({
-                thread_id: props.id,
-                space_id: props.id,
-            });
-            dispatch(action);
-        }
-    }, [Boolean(thread)]);
     return (
         <span
             className={clx(
