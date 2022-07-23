cd0ce2e888a274ca51e0308dc2e93a501dbbdef4
mfoncho <akonumboche@gmail.com>
2022-07-23 12:37:46 -0600

    update usercard ui use Dialog instead of Popper component wrapper

--- packages/webapp/src/workspace/UserCard/UserCard.tsx
+++ packages/webapp/src/workspace/UserCard/UserCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Popper, Textarea } from "@octal/ui";
+import { Button, Dialog, Textarea } from "@octal/ui";
 import { Text } from "@octal/ui";
-import PresenceAvatar from "../PresenceAvatar";
+import * as Icons from "@octal/icons";
 import { useInput } from "src/utils";
 import { useUser, useViewer, UserRecord } from "@octal/store";
 import * as ThreadAction from "@octal/store/lib/actions/thread";
@@ -34,15 +34,15 @@ const DirectMessageForm = React.memo<{
     return (
         <>
             <Textarea
-                value={input.value}
                 disabled={loading}
                 onSubmit={handleSubmit}
                 onChange={input.props.onChange}
-                className="text-msg p-3 min-h-[100px]"
+                className="text-msg p-3 min-h-[100px] flex-1 flex flex-row first-child:w-full"
                 placeholder={`@${user.username || ""} quick message`}
             />
             <button
-                className="bg-primary-500 rounded-b w-full text-white font-bold py-2"
+                disabled={loading}
+                className="disabled:bg-primary-400 bg-primary-700 w-full text-white font-bold py-2"
                 onClick={handleSubmit}>
                 Send
             </button>
@@ -50,27 +50,39 @@ const DirectMessageForm = React.memo<{
     );
 });
 
-const Card = Popper.create<HTMLDivElement, IUserCard>((props) => {
+const Card = Dialog.create<IUserCard>((props) => {
     const viewer = useViewer();
 
     const user = useUser(props.id)!;
     return (
-        <Popper
-            open={true}
-            anchorEl={props.anchorEl}
-            onClickAway={props.onClickAway}
-            className="rounded-md flex flex-col w-[250px] z-[1500] bg-white shadow-lg">
-            <div className="w-full flex py-5 justify-center items-center">
-                <PresenceAvatar
-                    id={user.id}
+        <Dialog.Base
+            open={props.open}
+            maxWidth="xs"
+            fullWidth={false}
+            fullScreen="mobile"
+            className="w-[280px] justify-between"
+            onClose={props.onClose}>
+            <div className="w-full flex flex-col justify-between overflow-hidden md:w-[280px] md:h-[250px] h-[350px] relative">
+                <img
+                    alt={user.username}
                     src={user.avatar}
-                    className={"w-20 h-20 rounded-full"}
+                    className={
+                        "w-full h-full md:rounded-t-lg absolute object-fill"
+                    }
                 />
-            </div>
-            <div className="flex flex-col">
-                <div className="flex flex-col justify-center items-center bg-gray-800/50">
+                <div className="flex px-4  py-4 flex-row justify-between md:invisible z-[1]">
+                    <div />
+
+                    <Button variant="icon" onClick={props.onClose}>
+                        <Icons.Close className="text-gray-500" />
+                    </Button>
+                </div>
+                <div
+                    className={`flex flex-col justify-center items-center bg-gray-800/50 w-full z-[1] py-2 backdrop-blur-sm  overflow-hidden ${
+                        viewer.id === user.id && "md:rounded-b-lg"
+                    }`}>
                     <span className="text-base font-bold text-white pb-px">
-                        {`@${user.name}`}
+                        {`@${user.username}`}
                     </span>
                     <span className="text-sm font-bold text-white pb-px">
                         {user.name}
@@ -81,14 +93,16 @@ const Card = Popper.create<HTMLDivElement, IUserCard>((props) => {
                         </span>
                     </span>
                 </div>
-                {viewer.id !== user.id && (
+            </div>
+            {viewer.id !== user.id && (
+                <div className="flex flex-col md:rounded-b-lg w-full overflow-hidden flex-1">
                     <DirectMessageForm
                         user={user}
-                        onSubmit={props.onClickAway as any}
+                        onSubmit={props.onClose as any}
                     />
-                )}
-            </div>
-        </Popper>
+                </div>
+            )}
+        </Dialog.Base>
     );
 });
 
@@ -114,8 +128,7 @@ function useCard(
             <Card
                 id={id}
                 open={Boolean(anchor) && Boolean(id)}
-                anchorEl={anchor}
-                onClickAway={() => setAnchor(null)}
+                onClose={() => setAnchor(null)}
             />
         );
     }
