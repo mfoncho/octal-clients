815943e4621d2c7ef3667a49b6385e35773723f0
mfoncho <akonumboche@gmail.com>
2022-09-09 00:21:10 -0600

    user card ui update

--- packages/webapp/src/workspace/UserCard/UserCard.tsx
+++ packages/webapp/src/workspace/UserCard/UserCard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Button, Dialog, Textarea } from "@octal/ui";
+import clx from "classnames";
+import { Button, Dialog, Textarea, Markdown, Flow } from "@octal/ui";
 import { Text } from "@octal/ui";
 import * as Icons from "@octal/icons";
 import { useInput } from "src/utils";
-import { useProfile, useViewer, UserRecord } from "@octal/store";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import { useProfile, useRoles, useViewer, UserRecord } from "@octal/store";
 import * as ThreadAction from "@octal/store/lib/actions/thread";
 import { useDispatch } from "react-redux";
 
@@ -32,35 +34,39 @@ const DirectMessageForm = React.memo<{
     }
 
     return (
-        <>
-            <Textarea
-                disabled={loading}
-                onSubmit={handleSubmit}
-                onChange={input.props.onChange}
-                className="text-msg p-3 min-h-[100px] flex-1 flex flex-row first-child:w-full"
-                placeholder={`@${user.username || ""} quick message`}
-            />
+        <div className="flex flex-1 flex-col overflow-hidden justify-between">
+            <PerfectScrollbar className="flex flex-col">
+                <Textarea
+                    disabled={loading}
+                    onSubmit={handleSubmit}
+                    onChange={input.props.onChange}
+                    className="text-msg p-3 min-h-[10px] flex-1 flex flex-row first-child:w-full"
+                    placeholder={`@${user.username || ""} quick message`}
+                />
+            </PerfectScrollbar>
             <button
                 disabled={loading}
                 className="disabled:bg-primary-400 bg-primary-700 w-full text-white font-bold py-2"
                 onClick={handleSubmit}>
                 Send
             </button>
-        </>
+        </div>
     );
 });
 
 const Card = Dialog.create<IUserCard>((props) => {
     const viewer = useViewer();
-
+    const roles = useRoles();
     const user = useProfile(props.id)!;
+    const [tab, setTab] = React.useState<string>("bio");
+    const userRoles = roles.filter((role) => user.roles.includes(role.id));
     return (
         <Dialog.Base
             open={props.open}
             maxWidth="xs"
             fullWidth={false}
             fullScreen="mobile"
-            className="w-[280px] justify-between"
+            className="relative w-[280px]"
             onClose={props.onClose}>
             <div className="w-full flex flex-col justify-between overflow-hidden md:w-[280px] md:h-[250px] h-[350px] relative">
                 <img
@@ -72,36 +78,90 @@ const Card = Dialog.create<IUserCard>((props) => {
                 />
                 <div className="flex px-4  py-4 flex-row justify-between md:invisible z-[1]">
                     <div />
-
                     <Button variant="icon" onClick={props.onClose}>
                         <Icons.Close className="text-gray-500" />
                     </Button>
                 </div>
-                <div
-                    className={`flex flex-col justify-center items-center bg-gray-800/50 w-full z-[1] py-2 backdrop-blur-sm  overflow-hidden ${
-                        viewer.id === user.id && "md:rounded-b-lg"
-                    }`}>
-                    <span className="text-base font-bold text-white pb-px">
-                        {`@${user.username}`}
-                    </span>
-                    <span className="text-sm font-bold text-white pb-px">
-                        {user.name}
-                    </span>
-                    <span className="text-sm font-semibold text-white py-px">
-                        <span>
-                            <Text>{user.status}</Text>
-                        </span>
+                <div className="flex flex-row justify-between items-center bg-gray-800/50 w-full z-[1] py-2 backdrop-blur-sm  overflow-hidden text-white px-4">
+                    <span className="text-sm font-semibold py-px">
+                        <Text>{user.status}</Text>
                     </span>
                 </div>
             </div>
-            {viewer.id !== user.id && (
-                <div className="flex flex-col md:rounded-b-lg w-full overflow-hidden flex-1">
-                    <DirectMessageForm
-                        user={user}
-                        onSubmit={props.onClose as any}
-                    />
+            <div
+                className={`flex flex-col justify-center py-2 px-4 text-gray-700 overflow-hidden ${
+                    viewer.id === user.id && "md:rounded-b-lg"
+                }`}>
+                <div className="flex flex-col">
+                    <span className="text-lg font-black pb-px">
+                        {user.name}
+                    </span>
+                    <span className="text-gray-600 text-sm font-bold pb-px">
+                        {`@${user.username}`}
+                    </span>
                 </div>
-            )}
+                <div className="space-x-2 flex flex-row flex-wrap py-2">
+                    {userRoles
+                        .map((role) => (
+                            <div
+                                key={role.id}
+                                className="text-sm font-semibold py-px space-x rounded-md border-slate-200 border bg-slate-100 hover:bg-slate-200">
+                                {role.icon.length > 0 && (
+                                    <span>
+                                        <Text>{role.icon}</Text>
+                                    </span>
+                                )}
+                                <span className="px-1">
+                                    <Text>{role.name}</Text>
+                                </span>
+                            </div>
+                        ))
+                        .toList()}
+                </div>
+                {/**
+                 **/}
+            </div>
+            <div className="flex flex-row h-12 bg-slate-100 z-[1] space-x-2 p-2">
+                <button
+                    onClick={(_) => setTab("bio")}
+                    className={clx(
+                        "flex-1 rounded-lg hover:bg-slate-200 py-4 text-gray-600 justify-center items-center flex",
+                        tab == "bio" && "bg-slate-300"
+                    )}>
+                    {tab == "bio" ? <Icons.Info.Solid /> : <Icons.Info />}
+                </button>
+                {viewer.id !== user.id && (
+                    <button
+                        onClick={(_) => setTab("chat")}
+                        className={clx(
+                            "flex-1 rounded-lg hover:bg-slate-200 py-4 text-gray-600 justify-center items-center flex",
+                            tab == "chat" && "bg-slate-300"
+                        )}>
+                        <Icons.Chat />
+                    </button>
+                )}
+            </div>
+            <div className="flex flex-col md:rounded-b-lg w-full overflow-hidden overflow-hidden md:h-[150px] flex-1 md:flex-none">
+                <Flow.Switch value={tab}>
+                    <Flow.Case value="chat">
+                        {viewer.id !== user.id && (
+                            <DirectMessageForm
+                                user={user}
+                                onSubmit={props.onClose as any}
+                            />
+                        )}
+                    </Flow.Case>
+                    <Flow.Case value="bio">
+                        <PerfectScrollbar className="flex flex-col">
+                            <div className="p-4 bg-slate-50 flex-1">
+                                <span className="text-base text-gray-700">
+                                    <Markdown>{user.bio}</Markdown>
+                                </span>
+                            </div>
+                        </PerfectScrollbar>
+                    </Flow.Case>
+                </Flow.Switch>
+            </div>
         </Dialog.Base>
     );
 });
