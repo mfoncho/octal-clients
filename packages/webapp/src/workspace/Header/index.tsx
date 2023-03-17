8e9a440101643a40832d9ef0ef333fd7a56b63a0
mfoncho <akonumboche@gmail.com>
2023-03-17 03:24:09 -0600

    [wip] add dark mode to home

--- packages/webapp/src/workspace/Header/index.tsx
+++ packages/webapp/src/workspace/Header/index.tsx
@@ -11,14 +11,14 @@ export default React.forwardRef<
     return (
         <header
             ref={ref}
-            className="flex flex-none border-b border-gray-200 flex-row items-center h-14 sm:h-20 flex-start sm:justify-between">
+            className="flex flex-none border-b border-gray-200 dark:border-slate-600 flex-row items-center h-14 sm:h-20 flex-start sm:justify-between">
             <div className="flex flex-row items-center sm:hidden pl-1 sm:pl-8 pr-2">
                 <Button
-                    variant="icon"
                     color="clear"
+                    variant="icon"
                     onClick={() => navigator.toggle({})}
                     className="visible sm:hidden">
-                    <Icons.Menu />
+                    <Icons.Menu className="dark:text-gray-100 dark:hover:text-white" />
                 </Button>
             </div>
             <div {...props}>{props.children}</div>
