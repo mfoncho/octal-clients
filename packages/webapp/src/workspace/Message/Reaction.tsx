529c696a0a73578d08ceb33a5a50ee046bd2d5c5
mfoncho <akonumboche@gmail.com>
2023-03-17 08:57:48 -0600

    [wip] add dark mode to message reaction

--- packages/webapp/src/workspace/Message/Reaction.tsx
+++ packages/webapp/src/workspace/Message/Reaction.tsx
@@ -13,8 +13,8 @@ export default React.memo(({ name, count, onClick, highlight }: IReaction) => {
     return (
         <button
             className={clx(
-                "flex rounded-xl m-1 p-0.5 hover:bg-gray-200 border border-gray-300 flex-row items-center",
-                highlight && "bg-gray-300"
+                "flex rounded-xl m-1 p-0.5 bg- hover:bg-gray-200 border border-slate-300 dark:border-slate-400 flex-row items-center",
+                highlight && "bg-slate-300 dark:bg-slate-500"
             )}
             onClick={onClick}>
             <img
@@ -22,7 +22,7 @@ export default React.memo(({ name, count, onClick, highlight }: IReaction) => {
                 className="w-5 h-5 rounded-full"
                 src={emoji.image(name)}
             />
-            <span className="px-1 font-bold text-xs">{count}</span>
+            <span className="px-1 font-bold text-xs text-gray-800 dark:text-slate-200">{count}</span>
         </button>
     );
 });
