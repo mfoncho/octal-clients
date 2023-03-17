08a049810b7044ec588ae91b8777a4af8252dd42
mfoncho <akonumboche@gmail.com>
2023-03-17 04:16:59 -0600

    [wip] add dark mode topic/thread Header

--- packages/webapp/src/workspace/Topic/Header.tsx
+++ packages/webapp/src/workspace/Topic/Header.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Dialog, Text, Button } from "@colab/ui";
 import * as Icons from "@colab/icons";
-import { useTopic, useSpacePermissions } from "@colab/store";
-import RenameTopicDialog from "./RenameTopicDialog";
+import Header from "../Header";
+import { useSpacePermissions, useThread } from "@colab/store";
+import RenameThreadDialog from "./RenameThreadDialog";
 import SearchDialog from "./Search";
 import { useParams } from "react-router-dom";
 import { useSpace } from "../Space/hooks";
-import { useNavigatorDrawer } from "src/hooks";
 
 export default React.memo(() => {
-    const [, navigator] = useNavigatorDrawer();
     const space = useSpace();
     const dialog = Dialog.useDialog();
     const params = useParams<{ topic_id: string }>();
@@ -20,30 +19,21 @@ export default React.memo(() => {
         dialog.close({} as any);
 
     return (
-        <header className="flex flex-none border-b border-gray-200 flex-row items-center h-14 sm:h-20 justify-between bg-primary-50">
-            <div className="flex flex-row items-center pl-1 sm:pl-8 pr-2">
-                <Button
-                    variant="icon"
-                    color="clear"
-                    onClick={() => navigator.toggle({})}
-                    className="visible sm:hidden">
-                    <Icons.Menu />
-                </Button>
-                <div className="flex flex-col justify-center px-2 overflow-hidden">
-                    <button
-                        onClick={dialog.opener("rename")}
-                        className="text-left ">
-                        <p className="truncate text-lg font-bold">
-                            {topic && <Text>{topic.name}</Text>}
-                        </p>
-                    </button>
-                </div>
+        <Header className="flex flex-grow flex-row items-center justify-between">
+            <div className="flex flex-col justify-center px-6 overflow-hidden">
+                <button
+                    onClick={dialog.opener("rename")}
+                    className="text-left ">
+                    <p className="truncate text-lg font-bold dark:text-gray-200">
+                        {thread && <Text>{space.name}</Text>}
+                    </p>
+                </button>
             </div>
             <div className="hidden sm:flex flex-none flex-row items-center justify-end px-4">
                 <div
                     role="button"
                     onClick={dialog.opener("search")}
-                    className="mx-4 text-gray-500 flex flex-grow items-center">
+                    className="mx-4 text-gray-500 dark:text-gray-400 flex flex-grow items-center">
                     <Icons.Search />
                     <span className="pl-4 font-semibold">
                         Quick search topic
@@ -64,6 +54,6 @@ export default React.memo(() => {
                     />
                 </>
             )}
-        </header>
+        </Header>
     );
 });
